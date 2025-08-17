6e16651f06b08565309ac51a658a4b31cbc95ba9
Nadia-Odame <n.odame@alustudent.com>
2025-08-17 06:00:54 -0700

    hm

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if 'a' <= char <= 'z':
+        if ord('a') <= ord(char) <= ord('z'):
             print("{}".format(chr(ord(char) - 32)), end="")
         else:
             print("{}".format(char), end="")
-    print()
+    print("")
