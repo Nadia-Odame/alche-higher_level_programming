9ad702a200adad1c99a9857d9b1d5bcbdab7c7af
Nadia-Odame <n.odame@alustudent.com>
2025-08-17 05:56:33 -0700

    hm

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Print a string in uppercase followed by a new line."""
     for char in str:
-        # Convert lowercase letters to uppercase using ASCII
         if 'a' <= char <= 'z':
             print("{}".format(chr(ord(char) - 32)), end="")
         else:
