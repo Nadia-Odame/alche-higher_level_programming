6549cba5ed61ded5c94e1bef6ec55204e41423ce
Nadia-Odame <n.odame@alustudent.com>
2025-09-06 09:58:11 -0700

    hm

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Defines a rectangle with width and height."""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
